cf8f9bf33c1a14a6ad8caac2e411252232dcd0ab
Med Rami Brick <brickrami1@gmail.com>
2024-02-25 14:41:22 +0100

    votes list back office

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RoomComponent } from './room/room.component';
-import { HomeComponent } from './home/home.component';
-import { VoteComponent } from './vote/components/vote.component';
 import { VotesListComponent } from './vote/votes-list/votes-list.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'room', component: RoomComponent},
-  {path: '', component: VoteComponent},
-  {path: 'votes', component: VotesListComponent},
-  
+  {path: '', component: VotesListComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { RoomComponent } from './room/room.component';
-import { VoteComponent } from './vote/components/vote.component';
+// import { HeaderComponent } from './frontoffice/header/header.component';
+// import { FooterComponent } from './frontoffice/footer/footer.component';
+// import { HomeComponent } from './frontoffice/home/home.component';
+// import { RoomComponent } from './room/room.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,20 +17,30 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
-import { TableRoomComponent } from './table-room/table-room.component';
+import { MatDialogModule } from '@angular/material/dialog';
+// import { TableRoomComponent } from './table-room/table-room.component';
 import { VotesListComponent } from './vote/votes-list/votes-list.component';
+import { ConfirmationComponent } from './vote/confirmation/confirmation.component';
+// import { VotebackComponent } from './backoffice/voteback/voteback.component';
+// import { FooterBackComponent } from './backoffice/components/footer/footerback.component';
+// import { NavbarComponent } from './backoffice/components/navbar/navbar.component';
+// import { SidebarComponent } from './backoffice/components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    RoomComponent,
-    VoteComponent,
-    TableRoomComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // HomeComponent,
+    // RoomComponent,
+    // TableRoomComponent,
     VotesListComponent,
+    ConfirmationComponent,
+    // VotebackComponent,
+    // FooterBackComponent,
+    // NavbarComponent,
+    // SidebarComponent
   ],
   imports: [
     BrowserModule,
@@ -43,6 +52,7 @@ import { VotesListComponent } from './vote/votes-list/votes-list.component';
     MatIconModule,
     MatSliderModule,
     MatSelectModule,
+    MatDialogModule,
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
