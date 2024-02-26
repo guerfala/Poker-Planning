44bcd3bccc1b6e2f4a9346d68b886b6c10b6d90b
guerfala <mohamedtaher.guerfala@esprit.tn>
2024-02-26 10:50:07 +0100

    room to merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RoomComponent } from './room/room.component';
-import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomComponent } from './room-poker/room/room.component';
+import { CreateRoomComponent } from './room-poker/create-room/create-room.component';
 import { HomeComponent } from './home/home.component';
-import { UpdateRoomComponent } from './update-room/update-room.component';
-import { TableRoomComponent } from './table-room/table-room.component';
+import { UpdateRoomComponent } from './room-poker/update-room/update-room.component';
+import { TableRoomComponent } from './room-poker/table-room/table-room.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
